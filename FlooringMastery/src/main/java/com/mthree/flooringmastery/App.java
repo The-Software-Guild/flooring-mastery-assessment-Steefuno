24fc963a10a0c68dde514f0c97fbd2a65f8c14d0
Steven <steven.686295@gmail.com>
2021-08-08 00:23:19 -0400

    Initial project setup

---
+++ FlooringMastery/src/main/java/com/mthree/flooringmastery/App.java
@@ -0,0 +1,15 @@
+package com.mthree.flooringmastery;
+
+/*
+ * @author Steven Nguyen
+ * @email: steven.686295@gmail.com
+ * @date: 
+ */
+
+/**
+ * 
+ * @author Steven
+ */
+public class App {
+
+}
