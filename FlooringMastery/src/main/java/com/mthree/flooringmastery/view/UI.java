40597990473f38cb5820a1b649bdce3972d2e53c
Steven <steven.686295@gmail.com>
2021-08-09 11:28:37 -0400

    Implemented UI and UIConsoleImplementation

--- FlooringMastery/src/main/java/com/mthree/flooringmastery/view/UI.java
+++ FlooringMastery/src/main/java/com/mthree/flooringmastery/view/UI.java
@@ -1,14 +1,33 @@
 /*
  * @author Steven Nguyen
  * @email: steven.686295@gmail.com
- * @date: 
+ * @date: 09 Aug 2021
  */
 package com.mthree.flooringmastery.view;
+
+import java.math.BigDecimal;
 
 /**
  *
  * @author Steven
  */
 public interface UI {
+    /**
+     * Displays a message to the user
+     * @param message the message to display
+     */
+    public void say(String message);
     
+    /**
+     * Reads a string inputted by the user
+     * @return the string
+     */
+    public String readString();
+    
+    /**
+     * Reads a number inputted by the user
+     * 2 decimal places
+     * @return the BigNumber
+     */
+    public BigDecimal readNumber();
 }
--- FlooringMastery/src/main/java/com/mthree/flooringmastery/view/UIConsoleImplementation.java
+++ FlooringMastery/src/main/java/com/mthree/flooringmastery/view/UIConsoleImplementation.java
@@ -1,10 +1,14 @@
 /*
  * @author Steven Nguyen
  * @email: steven.686295@gmail.com
- * @date: 
+ * @date: 09 Aug 2021
  */
 
 package com.mthree.flooringmastery.view;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
 
 
 /**
@@ -12,5 +16,46 @@ package com.mthree.flooringmastery.view;
  * @author Steven
  */
 public class UIConsoleImplementation implements UI {
-
+    final private Scanner inputReader;
+    
+    /**
+     * Constructs a new UIConsoleImplementation
+     */
+    public UIConsoleImplementation() {
+        inputReader = new Scanner(System.in);
+    }
+    
+    /**
+     * Displays a message to the user
+     * @param message the message to display
+     */
+    @Override
+    public void say(String message) {
+        System.out.println(message);
+    }
+    
+    /**
+     * Reads a string inputted by the user
+     * @return the string
+     */
+    @Override
+    public String readString() {
+        return inputReader.nextLine();
+    }
+    
+    /**
+     * Reads a number inputted by the user
+     * 2 decimal places
+     * @return the BigNumber
+     */
+    @Override
+    public BigDecimal readNumber() {
+        String line;
+        BigDecimal number;
+        
+        line = inputReader.nextLine();
+        number = (new BigDecimal(line)).setScale(2, RoundingMode.HALF_UP);
+        
+        return number;
+    }
 }
