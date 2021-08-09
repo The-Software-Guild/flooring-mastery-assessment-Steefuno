6586e0b5920ca23a92692be70419a770f2853d9c
Steven <steven.686295@gmail.com>
2021-08-09 10:55:24 -0400

    Setup Controller and implemented App.java

--- FlooringMastery/src/main/java/com/mthree/flooringmastery/App.java
+++ FlooringMastery/src/main/java/com/mthree/flooringmastery/App.java
@@ -1,15 +1,27 @@
-package com.mthree.flooringmastery;
-
 /*
  * @author Steven Nguyen
  * @email: steven.686295@gmail.com
- * @date: 
+ * @date: 09 Aug 2021
  */
+
+package com.mthree.flooringmastery;
+
+import com.mthree.flooringmastery.controller.Controller;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 
  * @author Steven
  */
 public class App {
-
+    public static void main(String[] args) {
+        ApplicationContext appContext;
+        Controller controller;
+            
+        appContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        controller = appContext.getBean("controller", Controller.class);
+        
+        controller.run();
+    }
 }
